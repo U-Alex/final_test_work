8550429a02fe3bf706b7fcd9c7cca4d8075f027a
UAA <uaa04@yandex.ru>
2022-12-01 21:36:26 +0900

    Добавил программу для обработки фиксированно заданного массива

--- Program.cs
+++ Program.cs
@@ -1,5 +1,19 @@
-﻿
+﻿string[] CreateNewArray(ref string[] array) {
+    int count = 0;
+    string[] newArr = new string[count];
+    foreach (string obj in array) {
+        if (obj.Length <= 3) {
+            count++;
+            Array.Resize(ref newArr, count);
+            newArr[count-1] = obj;
+        }
+    }
+    return newArr;
+}
 
 
+string[] array = {"hello", "2", "world", ":-)", "1234", "1567", "-2", "computer", "Russia", "Denmark", "Kazan"};
+string[] newArray =  CreateNewArray(ref array);
 
-
+Console.WriteLine($"исходный массив: [\"{String.Join("\", \"", array)}\"]");
+Console.WriteLine($"конечный массив: [\"{String.Join("\", \"", newArray)}\"]");
